dc3067e6c00bebb54709d56aeb12a0e72dddf657
jonsori <yohanees53@gmail.com>
2023-03-28 10:27:56 -0700

    Update 10

--- get_precision.c
+++ get_precision.c
@@ -10,32 +10,35 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+    int curr_i = *i + 1;
+    int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+    if (format[curr_i] != '.')
+        return (precision);
 
-	precision = 0;
+    precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+    curr_i++;
 
-	*i = curr_i - 1;
+    if (format[curr_i] == '*') {
+        curr_i++;
+        precision = va_arg(list, int);
+        *i = curr_i - 1;
+        return (precision);
+    }
 
-	return (precision);
+    while (format[curr_i] != '\0') {
+        if (is_digit(format[curr_i])) {
+            precision *= 10;
+            precision += format[curr_i] - '0';
+            curr_i++;
+        }
+        else {
+            break;
+        }
+    }
+
+    *i = curr_i - 1;
+
+    return (precision);
 }
